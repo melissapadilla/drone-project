cd09a7f39b1a823a550b959e62b2d0e32c1d80e7
melissapadilla <105305009+melissapadilla@users.noreply.github.com>
2022-05-12 09:52:41 -0700

    Create drone project .py

---
+++ drone project .py
@@ -0,0 +1,30 @@
+from djitellopy import tello
+from time import sleep
+
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+me.takeoff()
+#starting point
+me.move_up(80)
+me.move_forward(500)
+sleep(.5)
+#turning into "must fly zone"
+me.rotate_counter_clockwise(90)
+me.move_forward(300)
+sleep(.5)
+#turn into next square
+me.rotate_counter_clockwise(90)
+me.move_forward(500)
+sleep(.5)
+#back to start5
+me.rotate_counter_clockwise(90)
+me.move_forward(300)
+me.rotate_counter_clockwise(90)
+#turn forward
+me.rotate_counter_clockwise(90)
+me.move_forward(300)
+me.rotate_counter_clockwise(90)
+#landing
+sleep(.5)
+boof.land()
